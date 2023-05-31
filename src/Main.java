3dd46ee765d4c9d47a71f2cd804708dab908fa36
florinvardari <florin.vardari@gmail.com>
2023-05-30 21:21:43 -0400

    Minor change1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i <10 ; i++) {
-            System.out.println("Hello world");
-            Thread.sleep(2000);
+        for (int i = 0; i <20 ; i++) {
+            System.out.println("Hello world!");
+            Thread.sleep(1000);
         }
     }
 }
